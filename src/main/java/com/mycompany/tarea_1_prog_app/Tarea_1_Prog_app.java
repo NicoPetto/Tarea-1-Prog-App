ba0593e7ac5e9e92904978b89e3dd15d9f0a944f
NicoPetto <pettorossi.castillo@gmail.com>
2026-08-11 14:59:28 -0300

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/tarea_1_prog_app/Tarea_1_Prog_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_1_prog_app;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class Tarea_1_Prog_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
