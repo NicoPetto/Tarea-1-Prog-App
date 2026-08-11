d0c3978264c4a3a3867838d776d76597df57bd41
AdrianoTrinidad <adriano.trinidad@estudiantes.utec.edu.uy>
2026-08-11 15:47:08 -0300

    commit 1

--- src/main/java/com/mycompany/tarea_1_prog_app/Tarea_1_Prog_app.java
+++ src/main/java/com/mycompany/tarea_1_prog_app/Tarea_1_Prog_app.java
@@ -11,6 +11,6 @@ package com.mycompany.tarea_1_prog_app;
 public class Tarea_1_Prog_app {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!!");
     }
 }
